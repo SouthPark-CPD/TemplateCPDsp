b04bf6eba13acd51a74482eba59f4bf4e719b52f
Yvss91 <yassinbigpig@gmail.com>
2026-08-19 09:45:06 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,54 @@
+/* ==========================================================
+   DONNÉES — Procédures CPD
+   Fichier chargé dynamiquement par index.html via fetch().
+   Ce fichier n'apparaît PAS dans le "Ctrl+U" de index.html.
+========================================================== */
+
+window.CPD_DATA = [
+
+  {
+    id:"general",
+
+    label:"Général",
+
+    desc:"Procédures généraux utilisés dans le cadre des opérations du CPD.",
+
+    Procédures:[
+
+      {
+        number:"01",
+        title:"Compte rendu d'intervention",
+        type:"RAPPORT",
+        description:"Modèle permettant de rédiger un compte rendu complet à la suite d'une intervention.",
+        file:"https://docs.google.com/document/d/1VBaDAKFhK0FdDfAd1N_dADx8E1kCqZ7LCeYmLatr5DA/edit?usp=sharing"
+      },
+
+      {
+        number:"02",
+        title:"Rapport d'incident",
+        type:"RAPPORT",
+        description:"Procedure destiné à documenter un incident nécessitant un suivi administratif ou hiérarchique.",
+        file:"https://docs.google.com/document/d/1El4E35B-fStabW3TSJFoQIyvIJgCsr3G5vqPMZSLFRU/edit?usp=sharing"
+      },
+
+      {
+        number:"03",
+        title:"Rapport d'arrestation",
+        type:"RAPPORT",
+        description:"Document permettant de formaliser les circonstances et les éléments liés à une arrestation.",
+        file:"https://docs.google.com/document/d/1hBOVnF6BPqUjMbSW8coOeCMs060MN-1-6Zc_uVnMOAE/edit?usp=sharing"
+      },
+
+      {
+        number:"04",
+        title:"Droit Miranda",
+        type:"Droit",
+        description:"Procedure destiné à l'annonce des droits miranda",
+        file:"https://docs.google.com/document/d/1ohEObrjaYPbMRByq0nK7I8iS87Q_jDMFbNGEzmt3I_o/edit?usp=sharing"
+      }
+
+    ]
+
+  }
+
+];
